5d15fa19786850167a61646bd0f03816f9d54e44
Selbi <43946617+heyselbi@users.noreply.github.com>
2020-08-17 03:52:14 -0400

    Update download_dataset.sh

--- v0.5/classification_and_detection/download_dataset.sh
+++ v0.5/classification_and_detection/download_dataset.sh
@@ -1,3 +1,6 @@
+mkdir dataset
+cd dataset
+
 wget -q https://zenodo.org/record/3236545/files/resnet34-ssd1200.pytorch
 
 python -m pip install ck
@@ -10,7 +13,6 @@ ck install package --tags=image-classification,dataset,imagenet,val,original,ful
 ck install package --tags=image-classification,dataset,imagenet,aux
 
 ck locate env --tags=image-classification,dataset,imagenet,val,original,full
-/home/dvdt/CK-TOOLS/dataset-imagenet-ilsvrc2012-val
-$ ck locate env --tags=image-classification,dataset,imagenet,aux
-/home/dvdt/CK-TOOLS/dataset-imagenet-ilsvrc2012-aux
-$ cp `ck locate env --tags=aux`/val.txt `ck locate env --tags=val`/val_map.txt
+ck locate env --tags=image-classification,dataset,imagenet,aux
+
+cp `ck locate env --tags=aux`/val.txt `ck locate env --tags=val`/val_map.txt
