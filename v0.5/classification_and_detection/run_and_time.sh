f6f2ebd90fd979c31a776fbcb16375992a546d67
Selbi <43946617+heyselbi@users.noreply.github.com>
2020-08-17 03:58:58 -0400

    Update run_and_time.sh

--- v0.5/classification_and_detection/run_and_time.sh
+++ v0.5/classification_and_detection/run_and_time.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export MODEL_DIR=/root/v0.5/classification_and_detection/dataset
+export DATA_DIR=/root/v0.5/classification_and_detection/dataset
+
 source run_common.sh
 
 dockercmd=docker
@@ -15,12 +18,15 @@ if [ ! -d $OUTPUT_DIR ]; then
     mkdir -p $OUTPUT_DIR
 fi
 
-image=mlperf-infer-imgclassify-$device
-docker build  -t $image -f Dockerfile.$device .
+#image=mlperf-infer-imgclassify-$device
+#buildah bud --tls-verify=false  -t $image -f Dockerfile.$device .
+image=quay.io/selbi/inf-test
+
 opts="--config ./mlperf.conf --profile $profile $common_opt --model $model_path \
     --dataset-path $DATA_DIR --output $OUTPUT_DIR $extra_args $EXTRA_OPS $@"
 
-docker run $runtime -e opts="$opts" \
+buildah run $runtime -e opts="$opts" \
     -v $DATA_DIR:$DATA_DIR -v $MODEL_DIR:$MODEL_DIR -v `pwd`:/mlperf \
     -v $OUTPUT_DIR:/output -v /proc:/host_proc \
     -t $image:latest /mlperf/run_helper.sh 2>&1 | tee $OUTPUT_DIR/output.txt
+
