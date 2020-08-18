a61a97373be29498d33c79c58d9de91b0b7e9c37
Selbi <43946617+heyselbi@users.noreply.github.com>
2020-08-17 21:23:43 -0400

    Update run_local.sh

--- v0.5/classification_and_detection/run_local.sh
+++ v0.5/classification_and_detection/run_local.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
+export MODEL_DIR=/root/inference/v0.5/classification_and_detection/dataset
+export DATA_DIR=/root/inference/v0.5/classification_and_detection/dataset
+
 source ./run_common.sh
 
 common_opt="--config ../mlperf.conf"
 dataset="--dataset-path $DATA_DIR"
+
 OUTPUT_DIR=`pwd`/output/$name
 if [ ! -d $OUTPUT_DIR ]; then
     mkdir -p $OUTPUT_DIR
 fi
 
+echo "Clearing caches."
+sync && echo 3 | tee /host_proc/sys/vm/drop_caches
+
+cd /root
+
+common_opt=""
+
+start_fmt=$(date +%Y-%m-%d\ %r)
+echo "STARTING RUN AT $start_fmt"
+
 python python/main.py --profile $profile $common_opt --model $model_path $dataset \
     --output $OUTPUT_DIR $EXTRA_OPS $@
+
+end_fmt=$(date +%Y-%m-%d\ %r)
+echo "ENDING RUN AT $end_fmt"
