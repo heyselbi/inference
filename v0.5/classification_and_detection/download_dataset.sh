a9aa72882362b768ee92ce7eefeb759b980333fe
Selbi <43946617+heyselbi@users.noreply.github.com>
2020-08-17 21:28:10 -0400

    Update download_dataset.sh

--- v0.5/classification_and_detection/download_dataset.sh
+++ v0.5/classification_and_detection/download_dataset.sh
@@ -12,11 +12,17 @@ ck version
 
 ck pull repo:ck-env
 
+echo install
+
 ck install package --tags=image-classification,dataset,imagenet,val,original,full
 ck install package --tags=image-classification,dataset,imagenet,aux
 
+echo locate
+
 ck locate env --tags=image-classification,dataset,imagenet,val,original,full
 ck locate env --tags=image-classification,dataset,imagenet,aux
+
+echo copy
 
 cp `ck locate env --tags=aux`/val.txt `ck locate env --tags=val`/val_map.txt
 
