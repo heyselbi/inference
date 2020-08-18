33db26ffc7520032cd62d9bea6c330a3b0d343e0
Selbi <43946617+heyselbi@users.noreply.github.com>
2020-08-18 05:29:19 -0400

    Update download_dataset.sh

--- v0.5/classification_and_detection/download_dataset.sh
+++ v0.5/classification_and_detection/download_dataset.sh
@@ -26,15 +26,18 @@ echo "copy"
 
 cp `ck locate env --tags=aux`/val.txt `ck locate env --tags=val`/val_map.txt
 
+pwd
 ls -lt
 
-pwd
-
+echo "coco"
 ck install package --tags=object-detection,dataset,coco,2017,val,original
 ck locate env --tags=object-detection,dataset,coco,2017,val,original
 
+pwd
 ls -lt
 
-pwd
-
+ls -lt /tekton/home/CK-TOOLS/
+ls -lt /tekton/home/CK-TOOLS/dataset-coco-2017-val
+ls -lt /root/v0.5/classification_and_detection/
+ls -lt /root/v0.5/classification_and_detection/dataset/
 
