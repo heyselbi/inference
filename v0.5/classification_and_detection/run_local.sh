510c91b3df30d4dd07778941c594ec8168d4e206
Selbi <43946617+heyselbi@users.noreply.github.com>
2020-08-17 23:31:11 -0400

    Update run_local.sh

--- v0.5/classification_and_detection/run_local.sh
+++ v0.5/classification_and_detection/run_local.sh
@@ -18,9 +18,6 @@ if [ ! -d $OUTPUT_DIR ]; then
     mkdir -p $OUTPUT_DIR
 fi
 
-echo "Clearing caches."
-sync && echo 3 | tee /host_proc/sys/vm/drop_caches
-
 cd /root
 
 pwd
@@ -29,7 +26,7 @@ start_fmt=$(date +%Y-%m-%d\ %r)
 echo "STARTING RUN AT $start_fmt"
 
 echo "python_main"
-python python/main.py --profile $profile $common_opt --model $model_path $dataset \
+python v0.5/classification_and_detection/python/main.py --profile $profile $common_opt --model $model_path $dataset \
     --output $OUTPUT_DIR $EXTRA_OPS $@
 
 end_fmt=$(date +%Y-%m-%d\ %r)
